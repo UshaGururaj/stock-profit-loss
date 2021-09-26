e351e75950bc37e8d6b6db31e0347626896a991f
UshaGururaj <usha.neogrammer@gmail.com>
2021-09-26 15:15:55 +0530

    fix : added validation
    Added validation to restrict negative values input.

--- index.js
+++ index.js
@@ -15,23 +15,39 @@ function submitHandler(){
 
 function calculateProfitAndLoss(initial, quantity, current){
     if(initial && quantity && current){
-        if(initial > current){
-            bodyBackground.style.backgroundColor = "red";
-            let loss = (initial - current) * quantity;
-            let lossPercentage = (loss / initial) * 100;
-            showMessage(`Hey, the loss is ${loss} and the percent is ${lossPercentage}% `);
-        } else if(current > initial){
-            bodyBackground.style.backgroundColor = "green";
-            let profit = (current - initial) * quantity;
-            let profitPercentage = (profit / initial) * 100;
-            showMessage(`Hey, the profit is ${profit} and the percent is ${profitPercentage}%`);
-        } else{
-            bodyBackground.style.backgroundColor = "#595966";
-            showMessage("No pain no gain, and no gain no pain")
+        if((Number(initial)<=0) || (Number(quantity)<=0) || (Number(current)<=0)){
+
+            showMessage("Please enter valid values. Values should be greater than 0."); 
+
+        } else {
+
+            if(Number(initial) > Number(current)){
+
+                bodyBackground.style.backgroundColor = "red";
+                let loss = (Number(initial) - Number(current)) * Number(quantity);
+                let lossPercentage = (Number(loss) / Number(initial)) * 100;
+                showMessage(`Hey, the loss is ${loss} and the percent is ${lossPercentage}% `);
+        
+            } else if(Number(current) > Number(initial)){
+
+                bodyBackground.style.backgroundColor = "green";
+                let profit = (Number(current) - Number(initial)) * Number(quantity);
+                let profitPercentage = (Number(profit) / Number(initial)) * 100;
+                showMessage(`Hey, the profit is ${profit} and the percent is ${profitPercentage}%`);
+            
+            } else{
+
+                bodyBackground.style.backgroundColor = "#595966";
+                showMessage("No pain no gain, and no gain no pain");
+
+            }
         }
+
     } else {
+
         bodyBackground.style.backgroundColor = "#595966";
         showMessage("Please fill out all fields.");
+
     }
 }
 
